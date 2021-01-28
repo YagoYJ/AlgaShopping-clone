30fb09dfc0768dc8fca14fb3a2045802c4e81e2a
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2021-01-27 22:23:01 -0300

    Initial Redux settings

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
-import bg from '../../assets/bg.svg'
+import styled from "styled-components";
+import bg from "../../assets/bg.svg";
 
 export const Wrapper = styled.div`
   height: 100vh;
 
-  background-color: #F8F5EC;
+  background-color: #f8f5ec;
   background-image: url(${bg});
   background-size: cover;
   background-position: center center;
@@ -12,14 +12,14 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 export const Container = styled.div`
   width: 734px;
   height: 574px;
 
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 
-  box-shadow: 0 3px 10px rgba(0,0,0,.15);
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
   border-radius: 20px;
-`
+`;
