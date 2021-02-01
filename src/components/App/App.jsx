5023a774752c6bffdb555b6ca0e27a5640bc8469
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2021-01-31 22:18:09 -0300

    Adding redux to list of products and to total price

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import LineChart from "../../shared/LineChart";
 import AppContainer from "../AppContainer/AppContainer";
 import AppHeader from "../AppHeader";
 import ShoppingList from "../ShoppingList";
-import { Wrapper, Container } from "./App.styles";
 import extractPercentage from "../../utils/extractPercentage";
 import Calculator from "../Calculator";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAllProducts } from "../../store/Products/Products.selectors";
+
+import {
+  selectAllProducts,
+  selectSelectedProducts,
+  selectSelectedProductsPrice,
+} from "../../store/Products/Products.selectors";
 import { toggleProduct } from "../../store/Products/Products.actions";
+
+import { Wrapper, Container } from "./App.styles";
 
 function App() {
   const products = useSelector(selectAllProducts);
+  const selectedProducts = useSelector(selectSelectedProducts);
+  const totalPrice = useSelector(selectSelectedProductsPrice);
+
   const dispatch = useDispatch();
   const colors = ["#62CBC6", "#00ABAD", "#00858C", "#006073", "#004D61"];
-
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  useEffect(() => {
-    const newSelectedProducts = products.filter((product) => product.checked);
-
-    setSelectedProducts(newSelectedProducts);
-  }, [products]);
-
-  useEffect(() => {
-    const total = selectedProducts
-      .map((product) => product.price)
-      .reduce((a, b) => a + b, 0);
-
-    setTotalPrice(total);
-  }, [selectedProducts]);
 
   function handleToggle(id) {
     dispatch(toggleProduct(id));
--- src/store/Products/Products.selectors.js
+++ src/store/Products/Products.selectors.js
@@ -1,1 +1,9 @@
 export const selectAllProducts = (state) => state.products;
+
+export const selectSelectedProducts = (state) =>
+  state.products.filter((product) => product.checked);
+
+export const selectSelectedProductsPrice = (state) =>
+  state.products
+    .filter((product) => product.checked)
+    .reduce((a, b) => a + b.price, 0);
